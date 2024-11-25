4e85bf351109e3576ee4961297a2c8c4333eb2b9
nawen <GranatoM@outlook.it>
2024-11-25 13:45:55 +0100

    Prima implementazione SES

---
+++ src/main/java/com/prova/e_commerce/Config/AwsSesConfig.java
@@ -0,0 +1,31 @@
+package com.prova.e_commerce.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public SesClient sesClient() {
+        return SesClient.builder()
+                .region(Region.of(region))  // Imposta la regione AWS
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))  // Imposta le credenziali AWS
+                .build();
+    }
+}
